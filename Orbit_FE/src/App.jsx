f651b6028abb8fa5dba23ba604af027c7ca733aa
Shaany Gangle <shaanygangle21@gmail.com>
2026-07-13 23:13:52 +0530

    ADDED Validators

--- Orbit_FE/src/App.jsx
+++ Orbit_FE/src/App.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
+
+const Home = lazy(() => import("./pages/Home"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Login = lazy(() => import("./pages/Login"));
+const Groups = lazy(() => import("./pages/Groups"));
+
+let user = false;
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="about" element={<h1>About</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- Orbit_FE/src/pages/Login.jsx
+++ Orbit_FE/src/pages/Login.jsx
@@ -1,10 +1,36 @@
 import React, { useState } from "react";
-import { Container, Paper, TextField, Typography } from "@mui/material";
+import {
+  Container,
+  Paper,
+  TextField,
+  Typography,
+  Stack,
+  Avatar,
+  IconButton,
+} from "@mui/material";
+import { CameraAlt as CameraAltIcon } from "@mui/icons-material";
+import { VisuallyHiddenInput } from "../components/styles/StyledComponents";
+import { useFileHandler, useInputValidation } from "6pp";
+import { usernameValidator } from "../utils/validators";
 
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
 
   const toggleLogin = () => setIsLogin((prev) => !prev);
+
+  const name = useInputValidation("");
+  const bio = useInputValidation("");
+  const username = useInputValidation("", usernameValidator);
+  const password = useInputValidation("");
+
+  const avatar = useFileHandler("single");
+
+  const handlelogin = (e) => {
+    e.preventDefault();
+  };
+  const handleSignup = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <Container
@@ -32,13 +58,15 @@ const Login = () => {
             <Typography variant="h5" className="text-blue-600">
               Login
             </Typography>
-            <form className="w-96" action="">
+            <form onSubmit={handlelogin} className="w-96" action="">
               <TextField
                 required
                 fullWidth
                 label="User Name"
                 margin="normal"
                 variant="outlined"
+                value={username.value}
+                onChange={username.changeHandler}
               ></TextField>
 
               <TextField
@@ -47,21 +75,23 @@ const Login = () => {
                 label="Password"
                 margin="normal"
                 variant="outlined"
+                value={password.value}
+                onChange={password.changeHandler}
               ></TextField>
-            </form>
 
-            <button
-              sx={{
-                marginTop: "1 rem",
-              }}
-              fullWidth
-              varient="contained"
-              color="primary"
-              type="submit"
-              className=" mt-4 w-96 rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700 "
-            >
-              Login
-            </button>
+              <button
+                sx={{
+                  marginTop: "1 rem",
+                }}
+                fullWidth
+                variant="contained"
+                color="primary"
+                type="submit"
+                className=" mt-4 w-96 rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700 "
+              >
+                Login
+              </button>
+            </form>
 
             <Typography className="!mt-2 !text-sm text-gray-500">OR</Typography>
 
@@ -74,7 +104,7 @@ const Login = () => {
               onClick={toggleLogin}
               className="mt-2 w-96 text-blue-400"
             >
-              Sign In
+              Sign Up
             </button>
           </>
         ) : (
@@ -82,13 +112,60 @@ const Login = () => {
             <Typography variant="h5" className="text-blue-600">
               Sign Up
             </Typography>
-            <form className="w-96" action="">
+
+            <form onSubmit={handleSignup} className="w-96" action="">
+              <Stack className=" items-center relative mt-4 mx-32">
+                <Avatar
+                  sx={{
+                    height: "7rem",
+                    objectFit: "cover",
+                  }}
+                  className="!w-[7rem]"
+                  src={avatar.preview}
+                />
+
+                <IconButton
+                  sx={{
+                    position: "absolute",
+                    bottom: 0,
+                    right: 0,
+                    bgcolor: "background.paper",
+                    boxShadow: 2,
+                    "&:hover": {
+                      bgcolor: "grey.200",
+                    },
+                  }}
+                  component="label"
+                  className="absolute w-8 h-8"
+                >
+                  <>
+                    <CameraAltIcon />
+                    <VisuallyHiddenInput
+                      type="file"
+                      onChange={avatar.changeHandler}
+                    />
+                  </>
+                </IconButton>
+              </Stack>
+
+              {avatar.error && (
+                <Typography
+                  className="!mt-2.5 flex justify-center"
+                  color="error"
+                  variant="caption"
+                >
+                  {avatar.error}
+                </Typography>
+              )}
+
               <TextField
                 required
                 fullWidth
                 label="Full Name"
                 margin="normal"
                 variant="outlined"
+                value={name.value}
+                onChange={name.changeHandler}
               ></TextField>
 
               <TextField
@@ -97,6 +174,8 @@ const Login = () => {
                 label="Bio"
                 margin="normal"
                 variant="outlined"
+                value={bio.value}
+                onChange={bio.changeHandler}
               ></TextField>
 
               <TextField
@@ -105,28 +184,39 @@ const Login = () => {
                 label="Username"
                 margin="normal"
                 variant="outlined"
+                value={username.value}
+                onChange={username.changeHandler}
               ></TextField>
+
+              {username.error && (
+                <Typography color="error" variant="caption">
+                  {username.error}
+                </Typography>
+              )}
+
               <TextField
                 required
                 fullWidth
                 label="Password"
                 margin="normal"
                 variant="outlined"
+                value={password.value}
+                onChange={password.changeHandler}
               ></TextField>
-            </form>
 
-            <button
-              sx={{
-                marginTop: "1 rem",
-              }}
-              fullWidth
-              varient="contained"
-              color="primary"
-              type="submit"
-              className=" mt-4 w-96 rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700 "
-            >
-              Sign Up
-            </button>
+              <button
+                sx={{
+                  marginTop: "1 rem",
+                }}
+                fullWidth
+                variant="contained"
+                color="primary"
+                type="submit"
+                className=" mt-4 w-96 rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700 "
+              >
+                Sign Up
+              </button>
+            </form>
 
             <Typography className="!mt-2 !text-sm text-gray-500">
               Already have account ?
